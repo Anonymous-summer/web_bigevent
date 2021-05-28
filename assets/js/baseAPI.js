929b97ed0596b40b5b41c8d39e8b451763f1d8e3
summer <997783834@qq.com>
2021-05-28 15:54:32 +0800

    index第一次上传

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,16 @@
-$.ajaxPrefilter(function (options) {
-    options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+$.ajaxPrefilter(function (option) {
+    option.url = 'http://api-breakingnews-web.itheima.net' + option.url
+
+    if (option.url.includes('/my/') !== -1) {
+        option.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    option.complete = function (res) {
+        if (res.responseJSON.status == 1 && res.responseJSON.message == '身份认证失败！') {
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+        }
+    }
 })
